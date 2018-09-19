d6ed0174ccbc4ceb44d8899711637146537354c2
yuanxiwang <yuanxiwang@doctorwork.com>
2018-09-19 20:19:57 +0800

    init

--- src/main/java/com/rms/risproject/websocket/WebSocket.java
+++ src/main/java/com/rms/risproject/websocket/WebSocket.java
@@ -1,78 +1,104 @@
 package com.rms.risproject.websocket;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
-import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 @Log4j
+@ServerEndpoint(value = "/websocket")
 @Component
-@ServerEndpoint("/websocket")
 public class WebSocket {
+    //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
     private static int onlineCount = 0;
-    private static Map<String, WebSocket> clients = new ConcurrentHashMap<String, WebSocket>();
+    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
+    private static ConcurrentHashMap<String,WebSocket> webSocketSet = new ConcurrentHashMap<String,WebSocket>();
+
+    //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
-    private String username;
 
-    //@PathParam("username") String username, Session session @PathParam("username") String username,
+    /**
+     * 连接建立成功调用的方法
+     */
     @OnOpen
-    public void onOpen(Session session) throws IOException {
-
-        //this.username = username;
+    public void onOpen(Session session) {
         this.session = session;
-
-        addOnlineCount();
-        clients.put(username, this);
-        System.out.println("已连接");
-    }
-
-    @OnClose
-    public void onClose() throws IOException {
-        clients.remove(username);
-        subOnlineCount();
-    }
-
-    @OnMessage
-    public void onMessage(String message, Session session) throws Exception {
-        log.debug("收到websocket消息:" + message);
-        //尿检项目此处直接转发请求到前段
-        JSONObject jsonTo= JSON.parseObject(message);
-
-        if (!jsonTo.get("To").equals("All")){
-            sendMessageTo("给一个人", jsonTo.get("To").toString());
-        }else{
-            sendMessageAll("给所有人");
+        String macId= UUID.randomUUID().toString().replace("-","");
+        webSocketSet.put(macId,this);     //加入set中
+        addOnlineCount();           //在线数加1
+        log.info("有新连接加入！当前在线人数为" + getOnlineCount());
+        try {
+            sendMessage("连接成功");
+        } catch (IOException e) {
+            log.error("websocket IO异常");
         }
     }
 
+    /**
+     * 连接关闭调用的方法
+     */
+    @OnClose
+    public void onClose() {
+        webSocketSet.remove(this);  //从set中删除
+        subOnlineCount();           //在线数减1
+        log.info("有一连接关闭！当前在线人数为" + getOnlineCount());
+    }
+
+    /**
+     * 收到客户端消息后调用的方法
+     *
+     * @param message 客户端发送过来的消息
+     */
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        log.info("来自客户端的消息:" + message);
+
+        log.info(""+this);
+        //群发消息
+        for (String item : webSocketSet.keySet()) {
+            try {
+                webSocketSet.get(item).sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * @param session
+     * @param error
+     */
     @OnError
     public void onError(Session session, Throwable error) {
+        log.error("发生错误");
         error.printStackTrace();
     }
 
-    public void sendMessageTo(String message, String To) throws IOException {
-        // session.getBasicRemote().sendText(message);
-        //session.getAsyncRemote().sendText(message);
-        for (WebSocket item : clients.values()) {
-            if (item.username.equals(To) )
-                item.session.getAsyncRemote().sendText(message);
-        }
-    }
 
-    public void sendMessageAll(String message) throws IOException {
-        for (WebSocket item : clients.values()) {
-            item.session.getAsyncRemote().sendText(message);
-        }
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
     }
 
 
+    /**
+     * 群发自定义消息
+     */
+    public static void sendInfo(String message) throws IOException {
+        log.info(message);
+        for (String item : webSocketSet.keySet()) {
+            try {
+                webSocketSet.get(item).sendMessage(message);
+            } catch (IOException e) {
+                continue;
+            }
+        }
+    }
 
     public static synchronized int getOnlineCount() {
         return onlineCount;
@@ -84,9 +110,5 @@ public class WebSocket {
 
     public static synchronized void subOnlineCount() {
         WebSocket.onlineCount--;
-    }
-
-    public static synchronized Map<String, WebSocket> getClients() {
-        return clients;
     }
 }
